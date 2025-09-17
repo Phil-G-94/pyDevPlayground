8974e5d3f57f11ff95c5450bd3f7639413793290
Philippos Georgiou <philipposgeorgiou7@gmail.com>
2025-09-17 15:46:24 +0100

    Add more views

--- pyWebDev/djangoTutorial/polls/urls.py
+++ pyWebDev/djangoTutorial/polls/urls.py
@@ -4,6 +4,10 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns: List[URLPattern] = [
-    # defines a url `index` and maps it against the index fn defined in views.py
-    path("", views.index, name="index")
+    path(
+        "", views.index, name="index"
+    ),  # defines a url `index` and maps it against the index fn defined in views.py
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- pyWebDev/djangoTutorial/polls/views.py
+++ pyWebDev/djangoTutorial/polls/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpRequest, HttpResponse
 
+
 # defines a fn `index` which returns the following HttpResponse
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, World. You're at the polls index.")
+
+
+def detail(request: HttpRequest, question_id: int):
+    return HttpResponse("You're looking at the question %s." % question_id)
+
+
+def results(request: HttpRequest, question_id: int):
+    response = "You're looking at the results of the question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request: HttpRequest, question_id: int):
+    return HttpResponse("You're voting on the question %s." % question_id)
